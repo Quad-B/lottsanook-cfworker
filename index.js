d10860614e838c3e9086377bda35980c643397aa
Phongsakorn Wisetthon <boy1556@hotmail.com>
2022-08-15 20:04:07 +0700

    ok

--- index.js
+++ index.js
@@ -2,25 +2,6 @@ import FastifyEdge from 'fastify-edge'
 
 const app = FastifyEdge()
 
-app.addHook('onSend', (req, reply, payload) => {
-  if (req.url === '/') {
-    return `${payload} World!`
-  }
-})
-
 app.get('/', (_, reply) => {
   reply.send('Hello')
-})
-
-app.get('/redirect', (_, reply) => {
-  reply.redirect('/')
-})
-
-app.get('/route-hook', {
-  onRequest (_, reply) {
-    reply.send('<b>Content from onRequest hook</b>')
-  },
-  handler (_, reply) {
-    reply.type('text/html')
-  }
 })
