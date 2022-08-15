b68c680c9d411a31e8663e318f029186553f17f3
Phongsakorn Wisetthon <boy1556@hotmail.com>
2022-08-15 22:57:42 +0700

    ok

--- index.js
+++ index.js
@@ -954,6 +954,7 @@ fastify.get('/lotnews', async (request, reply) => {
         const pubDate = news.eq(i).find('pubDate').text()
         const getimage = await fetch(link)
         const responimage = await getimage.text()
+        console.log(responimage)
         //console.log(image)
         //write to html file
         //fs.writeFileSync('news.html', responimage)
@@ -969,7 +970,7 @@ fastify.get('/lotnews', async (request, reply) => {
             }
         });*/
         //console.log($('picture > img').toArray()[0].attribs['data-src'])
-        const image = $('picture > img').toArray()[0].attribs['src']
+        const image = $('picture > img').toArray()[0].attribs['data-src']
         //loop imageurl
         /*for (let index = 0; index < imageurl.length; index++) {
             console.log(imageurl)
@@ -1051,7 +1052,7 @@ fastify.get('/lotnews', async (request, reply) => {
         const getimage = await fetch(link)
         const responimage = await getimage.text()
         const $ = cheerio.load(responimage)
-        const image = $('picture > img').toArray()[0].attribs['src']
+        const image = $('picture > img').toArray()[0].attribs['data-src']
         const json = {
             title: title,
             //remove \n and \t in string
