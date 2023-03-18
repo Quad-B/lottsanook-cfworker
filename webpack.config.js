0c6a826081049f1822d58f7c1e087774a2711843
Phongsakorn Wisetthon <boy1556@hotmail.com>
2023-03-18 11:16:05 +0700

    ok

--- webpack.config.js
+++ webpack.config.js
@@ -6,7 +6,7 @@ module.exports = {
   entry: "./index.js",
   mode: "production",
   output: {
-    path: path.resolve(__dirname, 'dist'),
+    path: path.resolve(__dirname, 'worker'),
     filename: "worker.js"
   },
   resolve: {
